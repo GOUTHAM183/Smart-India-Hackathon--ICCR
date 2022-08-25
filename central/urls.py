3cc6063c40d2aea738db9ecd6a3bfadbc19df5fd
Kruthika Vasisht <kruthikasvasisht@gmail.com>
2022-08-25 08:52:28 +0530

    SIH-Commit-1

---
+++ central/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+from .views import (
+    PostListView,
+    PostDeleteView,
+    PostDetailView,
+    PostUpdateView,
+    PostCreateView,
+    UserPostListView,
+) 
+
+urlpatterns = [
+    path("",views.home,name="home"),
+    path("about/", views.about, name="about"),
+    path("regional/",views.regional,name="regional"),
+    path("iccr/",views.iccr,name="iccr"),
+    path("user/<str:username>", UserPostListView.as_view(), name="user-posts"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
+]
+
+
---
+++ central/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.views.generic import (
+    ListView,
+    DeleteView,
+    UpdateView,
+    CreateView,
+    DetailView,
+)
+from .models import Post
+
+def home(request):
+    context = {"posts": Post.objects.all()}
+    return render(request, "central/home.html", context)
+
+def regional(request):
+    return render(request, "central/regional.html", {})
+
+def iccr(request):
+    return render(request, "central/ICCRDash.html", {})
+
+
+def about(request):
+    return render(request, "central/about.html", {"title": "About"})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = "blog/user_posts.html"  # <app>/<model>_<viewtype>.html
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get("username"))
+        return Post.objects.filter(author=user).order_by("-date_posted")
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
